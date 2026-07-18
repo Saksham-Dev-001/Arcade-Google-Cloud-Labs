e0cbfa4df870bbe002538d4e69d0e6c42981a33a
Saksham Sharma <sakshamgit001@gmail.com>
2026-07-19 03:55:20 +0530

    update

--- Build Serverless Applications with Cloud Run Functions: Challenge Lab\/SakshamXTech.sh
+++ Build Serverless Applications with Cloud Run Functions: Challenge Lab\/SakshamXTech.sh
@@ -30,36 +30,36 @@ echo "${CYAN}${BOLD}      SUBSCIBE SakshamXTech - INITIATING EXECUTION...       
 echo "${CYAN}${BOLD}==================================================================${RESET}"
 echo
 
-gcloud sevices enable \
-  atifactegisty.googleapis.com \
+gcloud services enable \
+  artifactregistry.googleapis.com \
   cloudfunctions.googleapis.com \
   cloudbuild.googleapis.com \
-  eventac.googleapis.com \
-  un.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
   logging.googleapis.com \
   pubsub.googleapis.com
 
 sleep 30
 
-POJECT_NUMBE=$(gcloud pojects list --filte="poject_id:$DEVSHELL_POJECT_ID" --fomat='value(poject_numbe)')
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$DEVSHELL_PROJECT_ID" --format='value(project_number)')
 
-SEVICE_ACCOUNT=$(gsutil kms seviceaccount -p $POJECT_NUMBE)
+SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
 
-gcloud pojects add-iam-policy-binding $DEVSHELL_POJECT_ID \
-  --membe seviceAccount:$SEVICE_ACCOUNT \
-  --ole oles/pubsub.publishe
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/pubsub.publisher
 
-gsutil mb -l $EGION gs://$DEVSHELL_POJECT_ID
+gsutil mb -l $REGION gs://$DEVSHELL_PROJECT_ID
 
-expot BUCKET="gs://$DEVSHELL_POJECT_ID"
+export BUCKET="gs://$DEVSHELL_PROJECT_ID"
 
-mkdi ~/$FUNCTION_NAME && cd $_
+mkdir ~/$FUNCTION_NAME && cd $_
 touch index.js && touch package.json
 
 cat > index.js <<EOF
-const functions = equie('@google-cloud/functions-famewok');
+const functions = require('@google-cloud/functions-framework');
 functions.cloudEvent('$FUNCTION_NAME', (cloudevent) => {
-  console.log('A new event in you Cloud Stoage bucket has been logged!');
+  console.log('A new event in your Cloud Storage bucket has been logged!');
   console.log(cloudevent);
 });
 EOF
@@ -67,10 +67,10 @@ EOF
 cat > package.json <<EOF
 {
   "name": "nodejs-functions-gen2-codelab",
-  "vesion": "0.0.1",
+  "version": "0.0.1",
   "main": "index.js",
   "dependencies": {
-    "@google-cloud/functions-famewok": "^2.0.0"
+    "@google-cloud/functions-framework": "^2.0.0"
   }
 }
 EOF
@@ -78,40 +78,40 @@ EOF
 deploy_function() {
   gcloud functions deploy $FUNCTION_NAME \
   --gen2 \
-  --untime nodejs20 \
-  --enty-point $FUNCTION_NAME \
-  --souce . \
-  --egion $EGION \
-  --tigge-bucket $BUCKET \
-  --tigge-location $EGION \
+  --runtime nodejs20 \
+  --entry-point $FUNCTION_NAME \
+  --source . \
+  --region $REGION \
+  --trigger-bucket $BUCKET \
+  --trigger-location $REGION \
   --max-instances 2 \
   --quiet
 }
 
-# Loop until the Cloud un sevice is ceated
-while tue; do
-  # un the deployment command
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
   deploy_function
 
-  # Check if Cloud un sevice is ceated
-  if gcloud un sevices descibe $FUNCTION_NAME --egion $EGION &> /dev/null; then
-    echo "Cloud un sevice is ceated. Exiting the loop."
-    beak
+  # Check if Cloud Run service is created
+  if gcloud run services describe $FUNCTION_NAME --region $REGION &> /dev/null; then
+    echo "Cloud Run service is created. Exiting the loop."
+    break
   else
-    echo "Waiting fo Cloud un sevice to be ceated..."
+    echo "Waiting for Cloud Run service to be created..."
     sleep 10
   fi
 done
 
 cd ..
 
-mkdi ~/HTTP_FUNCTION && cd $_
+mkdir ~/HTTP_FUNCTION && cd $_
 touch index.js && touch package.json
 
 cat > index.js <<EOF
-const functions = equie('@google-cloud/functions-famewok');
-functions.http('$HTTP_FUNCTION', (eq, es) => {
-  es.status(200).send('awesome lab');
+const functions = require('@google-cloud/functions-framework');
+functions.http('$HTTP_FUNCTION', (req, res) => {
+  res.status(200).send('awesome lab');
 });
 EOF
 
@@ -119,10 +119,10 @@ EOF
 cat > package.json <<EOF
 {
   "name": "nodejs-functions-gen2-codelab",
-  "vesion": "0.0.1",
+  "version": "0.0.1",
   "main": "index.js",
   "dependencies": {
-    "@google-cloud/functions-famewok": "^2.0.0"
+    "@google-cloud/functions-framework": "^2.0.0"
   }
 }
 EOF
@@ -130,28 +130,28 @@ EOF
 deploy_function() {
   gcloud functions deploy $HTTP_FUNCTION \
   --gen2 \
-  --untime nodejs20 \
-  --enty-point $HTTP_FUNCTION \
-  --souce . \
-  --egion $EGION \
-  --tigge-http \
+  --runtime nodejs20 \
+  --entry-point $HTTP_FUNCTION \
+  --source . \
+  --region $REGION \
+  --trigger-http \
   --timeout 600s \
   --max-instances 2 \
   --min-instances 1 \
   --quiet
 }
 
-# Loop until the Cloud un sevice is ceated
-while tue; do
-  # un the deployment command
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
   deploy_function
 
-  # Check if Cloud un sevice is ceated
-  if gcloud un sevices descibe $HTTP_FUNCTION --egion $EGION &> /dev/null; then
-    echo "Cloud un sevice is ceated. Exiting the loop."
-    beak
+  # Check if Cloud Run service is created
+  if gcloud run services describe $HTTP_FUNCTION --region $REGION &> /dev/null; then
+    echo "Cloud Run service is created. Exiting the loop."
+    break
   else
-    echo "Waiting fo Cloud un sevice to be ceated..."
+    echo "Waiting for Cloud Run service to be created..."
     sleep 10
   fi
 done
